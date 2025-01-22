7e39221e2bfeb739325c0f7007b0ed84bde0827d
RyRose <RyRose@users.noreply.github.com>
2025-01-22 00:50:48 -0600

    feat: nixos configuration.nix, permanent reload of nixos config

---
+++ utils/preload_nixos.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [ ! -e ~/.config/nixos/configuration.nix ]; then
+    echo "~/.config/nixos/configuration.nix does not exist, stow $(home) to make available"
+    exit 1
+fi
+
+set -eux
+
+sudo nixos-rebuild switch \
+    -I "nixos-config=${HOME}/.config/nixos/configuration.nix" \
+    -I "nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos" \
+    -I "/nix/var/nix/profiles/per-user/root/channels"

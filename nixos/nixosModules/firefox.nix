45da0fd92043d2797d2c7a0aee236e8fc524e93c
RyRose <RyRose@users.noreply.github.com>
2025-03-27 21:38:37 -0500

    feat: misc nix changes

--- nixos/nixosModules/firefox.nix
+++ nixos/nixosModules/firefox.nix
@@ -32,7 +32,7 @@ in
         };
         DisablePocket = true;
         DisableFirefoxAccounts = true;
-        DisableAccounts = false; # Sync account for bookmarks.
+        DisableAccounts = false; # Sync account for bookmarks and extensions.
         DisableFirefoxScreenshots = true;
         OverrideFirstRunPage = "";
         OverridePostUpdatePage = "";
@@ -40,59 +40,6 @@ in
         DisplayBookmarksToolbar = "always"; # "never" or "newtab"
         DisplayMenuBar = "default-off"; # "always", "never", "default-off", or "default-on"
         SearchBar = "unified"; # "unified" or "separate"
-
-        # ---- EXTENSIONS ----
-        # Check about:support for extension/add-on ID strings.
-        # Valid strings for installation_mode are "allowed", "blocked",
-        # "force_installed" and "normal_installed".
-        ExtensionSettings = {
-          "*".installation_mode = "blocked"; # blocks all addons except the ones specified below
-          # uBlock Origin
-          "uBlock0@raymondhill.net" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-            installation_mode = "force_installed";
-          };
-          # Bitwarden
-          "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
-            installation_mode = "force_installed";
-          };
-          # Dark Reader
-          "addon@darkreader.org" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
-            installation_mode = "force_installed";
-          };
-          # BetterTTV
-          "firefox@betterttv.net" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/betterttv/latest.xpi";
-            installation_mode = "force_installed";
-          };
-          # FrankerFaceZ
-          "frankerfacez@frankerfacez.com" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/frankerfacez/latest.xpi";
-            installation_mode = "force_installed";
-          };
-          # Old Reddit Redirect
-          "{9063c2e9-e07c-4c2c-9646-cfe7ca8d0498}" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/old_reddit_redirect/latest.xpi";
-            installation_mode = "force_installed";
-          };
-          # RES
-          "jid1-xUfzOsOFlzSOXg@jetpack" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/reddit_enhancement_suite/latest.xpi";
-            installation_mode = "force_installed";
-          };
-          # Tampermonkey
-          "firefox@tampermonkey.net" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/tampermonkey/latest.xpi";
-            installation_mode = "force_installed";
-          };
-          # Raindrop.io
-          "jid0-adyhmvsP91nUO8pRv0Mn2VKeB84@jetpack" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/raindropio/latest.xpi";
-            installation_mode = "force_installed";
-          };
-        };
 
         # ---- PREFERENCES ----
         # Check about:config for options.
